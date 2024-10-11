9264e00b6e14a9d088bb1e5d292c31a0c200fb49
George Barrett <georgeaustinbarrett@gmail.com>
2024-10-11 19:11:00 +0100

    current state of the database is reflected

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_11_180301) do
+  create_table "planets", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name"
+    t.string "planet_type"
+    t.integer "circumference"
+    t.integer "diameter"
+    t.string "mass"
+    t.boolean "has_atmosphere"
+    t.boolean "has_water"
+    t.boolean "has_ice"
+    t.boolean "has_rings"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
