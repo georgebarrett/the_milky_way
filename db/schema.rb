cd6295b1b7db6218718e4ab0a66a843d03d215c3
George Barrett <georgeaustinbarrett@gmail.com>
2024-10-14 18:33:59 +0100

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_14_164327) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_14_172622) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -49,6 +49,15 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_14_164327) do
     t.boolean "has_water"
     t.boolean "has_ice"
     t.boolean "has_rings"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stellar_systems", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.integer "number_of_stars"
+    t.integer "number_of_planets"
+    t.integer "number_of_moons"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
