e60974369c5a5817d802d2bc7bcd35e6441b1094
George Barrett <georgeaustinbarrett@gmail.com>
2024-10-16 08:40:46 +0100

    linting issue fixed

--- spec/models/stellar_system_spec.rb
+++ spec/models/stellar_system_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe StellarSystem, type: :model do
     it'is a valid solar system with valid attributes' do
       stella_system = StellarSystem.new(
-        name: 'Alpha Centuri',
+        name: "Alpha Centuri",
         number_of_stars: 3,
         number_of_planets: 5,
         number_of_moons: 10
